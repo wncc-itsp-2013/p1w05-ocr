04544bcdd3ef04e0e7554cd9516456c18bc91308
alokminesh <alokminesh@gmail.com>
2013-06-01 16:17:43 +0530

    Added function documentation for segmentation.m

--- segmentation.m
+++ segmentation.m
@@ -16,3 +16,26 @@ for i=1:70
     filename = sprintf('ocrfonts_images/label_%d.bmp',i);
     imwrite(subimage,filename,'bmp');
 end
+
+%%%%%%%%%%%%%%%%%%%%%%% FUNCTION DOCUMENTATION %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%This code is a follow up on the boundbox.m file and uses the output
+%obtained from boundbox.m to segment the image 'ocrfonts.png'
+%maxx and maxy were found out to be 19 and 34 respectively and there were
+%70 components in total. 26+26(alphabets) + 10 digits + 8 special
+%characters.
+
+%imcrop function is used to crop an image matrix according to the input
+%parameters. The syntax used here is imcrop(image,map) where bw3 is the
+%image to be cropped and map is the dimensions and co-ordinates of the
+%bounding box.
+
+%imresize takes in an image matrix and resized dimensions as input and
+%returns the resized image. Here all segmented images are resized to 19X34
+
+%sprintf is used to name the files of imdividual images segmented from the
+%original image and its syntax is similar to other common programming
+%languages such as C++ etc.
+
+%imwrite is used to write an image matrix to a file and the syntax used
+%here is imwrite(image,filename,'file type).
